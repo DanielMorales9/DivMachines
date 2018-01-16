bug fix: wrong variance computation

divmachines/topk/__init__.py
@@ -101,6 +101,10 @@ class LatentFactorPortfolio(Classifier):
     def n_items(self):
         return self._model.n_items
 
+    @property
+    def logger(self):
+        return self._logger
+
     def _initialize(self):
         self._init_model()
 
@@ -288,14 +292,12 @@ class LatentFactorPortfolio(Classifier):
         x = self._model.x
         y = self._model.y
 
-        # computing user profile length
-        _, upl = np.unique(train[:, 0], return_counts=True)
-
-        upl = np.array(upl, dtype=np.float)
-        self._var = np.zeros((self.n_users, self._n_factors), dtype=np.float32)
+        self._var = np.zeros((self.n_users, self._n_factors),
+                             dtype=np.float32)
 
         for i, (u, g) in enumerate(pd.DataFrame(train).groupby(0)):
-            user_profile = g.values[0]
+            user_profile = g.values[:, 1]
+            upl = user_profile.shape[0]
             user_idx = Variable(gpu(torch.from_numpy(np.array([u])),
                                     self._use_cuda))
             item_idx = Variable(gpu(torch.from_numpy(user_profile),
@@ -326,5 +328,4 @@ def _substitute(arg_max_per_user, k, rank, rows):
     for r, c in zip(rows, arg_max_per_user):
         temp = rank[r, c + k]
         rank[r, c + k] = rank[r, k]
-        # TODO ValueError setting an array element with a sequence.
         rank[r, k] = temp

divmachines/topk/demo/movielens.py
@@ -1,20 +1,22 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 from divmachines.topk import LatentFactorPortfolio as LFP
 from divmachines.logging import TrainingLogger as TLogger
 from divmachines.utility.helper import cartesian
 
 cols = ['user', 'item', 'rating', 'timestamp']
-train = pd.read_csv('../../../data/ua.base', delimiter='\t', names=cols).sample(1000)
+train = pd.read_csv('../../../data/ua.base', delimiter='\t', names=cols).sample(10000)
 
 logger = TLogger()
 
-model = LFP(n_iter=100,
+model = LFP(n_iter=1000,
             n_jobs=4,
             n_factors=10,
-            batch_size=10000,
-            learning_rate=0.60653066,
+            batch_size=1000,
+            learning_rate=1,
             logger=logger)
+
 
 interactions = train[['user', 'item', 'rating']].values
 
@@ -28,6 +30,9 @@ x = interactions[:, :-1]
 y = interactions[:, -1]
 
 model.fit(x, y, n_users=n_users, n_items=n_items)
+
+plt.plot(logger.epochs, logger.losses)
+plt.show()
 
 values = cartesian(np.unique(x[:, 0]), np.unique(x[:, 1]))
 